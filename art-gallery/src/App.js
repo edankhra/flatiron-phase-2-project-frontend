1acd154aea14dead2d9b5c8fc8d14f126f530b92
Ekta <edankhra@gmail.com>
2023-10-01 13:51:12 -0400

    Have done some changes in App.js file

--- art-gallery/src/App.js
+++ art-gallery/src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Paintings from './components/Paintings';
+import PaintingList from './components/PaintingList';
+import PaintingForm from './components/PaintingForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
